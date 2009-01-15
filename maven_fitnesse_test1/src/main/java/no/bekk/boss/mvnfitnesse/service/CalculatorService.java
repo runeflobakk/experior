2c6aec25265d19bb80178dc377ce100ec4152bd2
flempeter <flempeter@68d355ac-de77-11dd-987e-d7c0a4ebcf36>
2009-01-15 08:30:15 +0000

    Dette er et testing av maven fitnesse som vart sendt til oss av Rune 15.01.2009. Det har blitt omgjort til et eclipse maven-fitnesse prosjekt og dermed importert i eclipse.

---
+++ maven_fitnesse_test1/src/main/java/no/bekk/boss/mvnfitnesse/service/CalculatorService.java
@@ -0,0 +1,26 @@
+package no.bekk.boss.mvnfitnesse.service;
+
+public class CalculatorService {
+
+    public int sum(int ... numbers) {
+        int sum = 0;
+        for(int number : numbers) {
+            sum += number;
+        }
+        return sum;
+    }
+
+
+
+    /**
+     * Ta med denne metoden for å tilfredsstille spesialhåndteringen av
+     * addisjon av 2 + 5, som skal bli 44.
+     */
+//    public int sum(int a, int b) {
+//        if (a == 2 && b == 5) {
+//            return 44;
+//        } else {
+//            return sum(new int[]{a, b});
+//        }
+//    }
+}
