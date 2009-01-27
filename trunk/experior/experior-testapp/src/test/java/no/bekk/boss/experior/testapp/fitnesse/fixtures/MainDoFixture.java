31884fb6870ac182f6118b8d3fd6db6ca4c3742d
flobakk <flobakk@68d355ac-de77-11dd-987e-d7c0a4ebcf36>
2009-01-27 22:35:33 +0000

    First commit. A test project to simulate "real life" use of the experior-editor-plugin.

---
+++ trunk/experior/experior-testapp/src/test/java/no/bekk/boss/experior/testapp/fitnesse/fixtures/MainDoFixture.java
@@ -0,0 +1,14 @@
+package no.bekk.boss.experior.testapp.fitnesse.fixtures;
+
+import no.bekk.boss.experior.testapp.service.CalculatorService;
+import fitlibrary.DoFixture;
+
+public class MainDoFixture extends DoFixture {
+
+    private CalculatorService calculator = new CalculatorService();
+
+    public boolean tallenePlusSkalBli(int a, int b, int expected) {
+        return calculator.sum(a, b) == expected;
+    }
+
+}
