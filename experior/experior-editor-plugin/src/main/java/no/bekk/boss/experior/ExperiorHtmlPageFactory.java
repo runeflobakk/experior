06a9afac8aea14485141f93384d167747343a916
flobakk <flobakk@68d355ac-de77-11dd-987e-d7c0a4ebcf36>
2009-01-27 22:21:54 +0000

    First commit. A working FitNesse plugin skeleton (HtmlPageFactory)

---
+++ experior/experior-editor-plugin/src/main/java/no/bekk/boss/experior/ExperiorHtmlPageFactory.java
@@ -0,0 +1,25 @@
+package no.bekk.boss.experior;
+
+import static fitnesse.html.HtmlUtil.makeJavascriptLink;
+
+import java.util.Properties;
+
+import fitnesse.html.HtmlPage;
+import fitnesse.html.HtmlPageFactory;
+
+/**
+ * @author Administrator
+ */
+public class ExperiorHtmlPageFactory extends HtmlPageFactory {
+
+    public ExperiorHtmlPageFactory(@SuppressWarnings("unused") Properties properties) {
+    }
+
+    @Override
+    public HtmlPage newPage() {
+        HtmlPage page = super.newPage();
+        page.head.add(makeJavascriptLink("Dette er vår egen javascript-path"));
+        return page;
+    }
+
+}
