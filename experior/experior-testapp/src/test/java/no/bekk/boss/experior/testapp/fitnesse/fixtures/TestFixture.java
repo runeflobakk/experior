d9e791cbac7cfc32d97925389a5898a9c2912d24
larsenrobert2305 <larsenrobert2305@68d355ac-de77-11dd-987e-d7c0a4ebcf36>
2009-04-29 09:32:50 +0000

    29/04/09 Improved method list and some bugfixes

---
+++ experior/experior-testapp/src/test/java/no/bekk/boss/experior/testapp/fitnesse/fixtures/TestFixture.java
@@ -0,0 +1,51 @@
+package no.bekk.boss.experior.testapp.fitnesse.fixtures;
+
+public class TestFixture extends MainDoFixture {
+	
+	 public boolean test1(int a, int b, int expected) {
+	        return false;//calculator.sum(a, b) == expected;
+	    }
+	    
+	    public boolean test2()
+	    {
+	    	return true;
+	    }
+	    
+	    public boolean test3()
+	    {
+	    	return true;
+	    }
+	    
+	    public boolean test4()
+	    {
+	    	return true;
+	    }
+	    
+	    public boolean test5()
+	    {
+	    	return true;
+	    }
+	    
+	    public boolean test6()
+	    {
+	    	return true;
+	    }
+	    
+	    public boolean test7()
+	    {
+	    	return true;
+	    }
+	    
+	    public boolean test8()
+	    {
+	    	return true;
+	    }
+
+	    
+	    public boolean test9()
+	    {
+	    	return true;
+	    }
+
+
+}
