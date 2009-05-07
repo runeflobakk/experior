3a76fb475b4e730a6723ff298ffa79a7ec7a5c80
larsenrobert2305 <larsenrobert2305@68d355ac-de77-11dd-987e-d7c0a4ebcf36>
2009-05-07 13:02:53 +0000

    07/05/09 Code-cleanup and dynamic resizing of Experior's size based on the clients resolution

--- experior/experior-testapp/src/test/fitnesse/root/files/javascript/experior.js
+++ experior/experior-testapp/src/test/fitnesse/root/files/javascript/experior.js
@@ -1,208 +1,184 @@
-
-var LINK_TEXT = "Experior";
-
-var MAX_ADD_LINK_TRIES = 3;
-var MILLISECOND_SLEEP = 200;
-var tries = 0;
-
-
-
-function GetAttributeValue( node, attribute ) {
-	for( var i=0;i<node.attributes.length;i++ ) {
-		if( node.attributes[i].nodeName.toUpperCase() == attribute.toUpperCase() )
-			return node.attributes[i].nodeValue;
-	}
-	return "";
-}
-
-function getDiv( tagName,attributeName,attributeValue,retryCall ) {
-	tries++;
-	var elements = document.getElementsByTagName( tagName ); 
+/*
+ * CodePress - Real Time Syntax Highlighting Editor written in JavaScript - http://codepress.org/
+ * 
+ * Copyright (C) 2006 Fernando M.A.d.S. <fermads@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the 
+ * GNU Lesser General Public License as published by the Free Software Foundation.
+ * 
+ * Read the full licence: http://www.opensource.org/licenses/lgpl-license.php
+ */
+var experior;
+var hidden;
+Experior = function(textarea, obj2) {
+	experior = document.createElement('iframe');
+	experior.textarea = textarea;
 	
-	for( var i=0; i<elements.length; i++ ) {
-		var value = GetAttributeValue( elements[i],attributeName );
-		if( value==attributeValue ) {
-			return elements[i];
-		}
+	hidden = obj2;		
+	experior.textarea.disabled = true;
+	experior.textarea.style.overflow = 'hidden';
+	experior.style.height = screen.height - 330 + 'px';//experior.textarea.clientHeight +'px';
+	experior.style.width = screen.width - 170 + 'px';//experior.textarea.clientWidth +'px';
+	experior.textarea.style.overflow = 'auto';
+	experior.style.border = '1px solid gray';
+	experior.frameBorder = 0; // remove IE internal iframe border
+	experior.style.visibility = 'hidden';
+	experior.style.position = 'absolute';
+	experior.options = experior.textarea.className;
+
+	experior.initialize = function() {
+
+		experior.editor = experior.contentWindow.Experior;
+		experior.contentWindow.focus();
+		experior.editor.body = experior.contentWindow.document.getElementsByTagName('body')[0];
+
+		experior.editor.alignStart( experior.textarea.value);
+		experior.setOptions();
+		experior.editor.syntaxHighlight('init', hidden.value); //hidden.value henter metodenavn fra hidden field
+		experior.textarea.style.display = 'none';
+		experior.style.position = 'static';
+		experior.style.visibility = 'visible';
+		experior.style.display = 'inline';		
+
+		var methods = hidden.value.split("\n");
+		experior.editor.createMethodsDiv( methods);
 	}
-	//still here, not found, try again.
-	if( tries<MAX_ADD_LINK_TRIES ) {
-		setTimeout( retryCall,MILLISECOND_SLEEP );
-	}
-}
 
-function AddExperiorLink(){
+	// obj can by a textarea id or a string (code)
+	experior.edit = function(textarea,language) {
+		if(textarea) experior.textarea.value = document.getElementById(textarea) ? document.getElementById(textarea).value : textarea;
+		if(!experior.textarea.disabled) return;
+		experior.language = language ? language : experior.getLanguage();
 
-		var editLink = getDiv("a","accesskey","e", AddExperiorLink);
-		if( editLink==null )
-			return;
-		
-		var div = getDiv("div","class","actions", AddExperiorLink);
-		
-		var link = document.createElement( 'A' );
-		link.innerHTML = LINK_TEXT;
-		
-		var pathname = location.pathname;
-		
-		if( pathname=="/" )
-			pathname += "FrontPage";
-		
-		link.href = pathname + "?Experior";
-		div.insertBefore( link,editLink );
-	
-}
+		experior.src = Experior.path+'experior.html?';
 
-AddExperiorLink();
-
-function readKey(e, textarea) {
-	 var oldcursorposition = getCursor(textarea);
-		
-		if (e.keyCode == '13'){
-		visPosisjon(textarea);
-		setCursor(textarea, oldcursorposition);
-		}
-
-	 }
-
-	 function getCursor(textarea){
-
-		var cursorPos = 0;
-	 if (document.selection)
-		{ 
-		textarea.focus();
-	 var tmpRange = document.selection.createRange();
-	 tmpRange.moveStart('character',-textarea.value.length);
-	 cursorPos = tmpRange.text.length;
-	 }
-	 else {
-		if (textarea.selectionStart || textarea.selectionStart == '0')
+		if(experior.attachEvent)
 		{
-		cursorPos = textarea.selectionStart;
+			experior.attachEvent('onload',experior.initialize);
 		}
+		else 
+		{
+			experior.addEventListener('load',experior.initialize,false);
 		}
-		return cursorPos;
-	 }
-
-
-	function setCursor(obj, pos) { 
-	 if(obj.createTextRange) { 
-	 /* Create a TextRange, set the internal pointer to
-	 a specified position and show the cursor at this
-	 position
-	 */ 
-	 var range = obj.createTextRange(); 
-	 range.move("character", pos); 
-	 range.select(); 
-	 } else if(obj.selectionStart) { 
-	 /* Gecko is a little bit shorter on that. Simply
-	 focus the element and set the selection to a
-	 specified position
-	 */ 
-	 obj.focus(); 
-	 obj.setSelectionRange(pos, pos); 
-	 } 
-	} 
-
-	 function isIE() {
-		var ie = (navigator.userAgent.indexOf("MSIE")==-1)?false:true;
-		return ie;
-	 }
-	 function visPosisjon( textarea )
-	 {
-
-	 var longest = 0;
-
-	 var tekst = textarea.value.split('\n'); //Splitter opp hver linje i hvert sitt arrayelement
-	 var substart,subend;
-	 var linjeArray = new Array();
-	 var template = new Array();
-	 var space;
-	 var content = ""; 
-	 debug("");
-	 for(var i=0, len = tekst.length; i < len; i++) { // Går igjennom arrayen (linjene)
-	 substart = 0;
-	 subend = 0;
-	 
-	 if (tekst[i].substr(0,1) == "|") //Hvis linja starter med | skal første tegn kuttes
-	 substart = 1;
-	 if (tekst[i].substr(tekst[i].length-1,tekst[i].length) == "|") // Hvis den slutter med | skal siste tegn kuttes
-	 subend = 1;
-
-	 linjeArray[i] = tekst[i].substring(substart,tekst[i].length-subend).split('|'); // Splitter linja til en array der "radene" ligger
-
-		
-	 for (var j=0, len2 = linjeArray[i].length; j < len2; j++) {
-	 linjeArray[i][j] = linjeArray[i][j].replace(/^\s+|\s+$/g, '');
-	 if (isIE() == true) {
-		if (linjeArray[i][j] == "") {linjeArray[i].pop();
-		debug("Test", true);}
-		}
-		//debug(linjeArray[i], true);
-	}
-		
-	 
-	 }
-	 
-	 for(var i=0, len = linjeArray.length; i < len; i++) {// Går igjennom hver linje
-	 for(var j=0, len2 = linjeArray[i].length; j < len2; j++) { // Går igjennom hvert element på linje
-	 if (template[j] == null)
-	 template[j] = 0;
-	 if (linjeArray[i][j].length > template[j]) {
-	 template[j] = linjeArray[i][j].length;
-	 }
-
-	 }
-	 }
-
-	 
-	 
-	 for(var i=0, len = linjeArray.length; i < len; i++) // Går igjennom hver linje
-	 { 
-	 
-		if (linjeArray[i][0] != "") {
-		for(var j=0, len2 = linjeArray[i].length; j < len2; j++) // Går igjennom hvert element på linja
-	 {
-	 space = "";
-	 
-	 for(var k=0, len3 = template[j] - linjeArray[i][j].length; k < len3; k++)
-	 space += " ";
-	 //debug("@"+linjeArray[i][j]+"@", true);
-	 content += "|" + linjeArray[i][j] + space; // Legger linja med mellomrommene inn i content-variablen
-	 
-		if (j == len2-1 && i != len-1)
-			content += "|\n"; 
-		else if (j == len2-1)
-			content+= "|";
-		
-	 }	
-		} else if (i != len-1) {
-			content += "\n";	
-		}
-	 
-
-
-	 }
-
-	 debug(nl2br(content));
-	 document.getElementById("ta").value = content; // Legger innholdet i variablen content inn i textarea
-	 
-	 
-	 }
-	 function nl2br(text){
-		text = escape(text);
-		if(text.indexOf('%0D%0A') > -1){
-			re_nlchar = /%0D%0A/g ;
-		}else if(text.indexOf('%0A') > -1){
-			re_nlchar = /%0A/g ;
-		}else if(text.indexOf('%0D') > -1){
-			re_nlchar = /%0D/g ;
-		}
-		return unescape( text.replace(re_nlchar,'<br />') );
 	}
 
-	 function debug(text, append) {
-	 if (append == true)
-	 document.getElementById("debug").value += text + "\n";
-	 else
-	 document.getElementById("debug").value = text;
-	 }
+	experior.getLanguage = function() {
+		for (language in Experior.languages) 
+			if(experior.options.match('\\b'+language+'\\b')) 
+				return ExperiorPress.languages[language] ? language : 'generic';
+	}
+
+	experior.setOptions = function() {
+		if(experior.options.match('autocomplete-off')) experior.toggleAutoComplete();
+		if(experior.options.match('readonly-on')) experior.toggleReadOnly();
+		if(experior.options.match('linenumbers-off')) experior.toggleLineNumbers();
+	}
+
+	experior.getCode = function() {
+		return experior.textarea.disabled ? experior.editor.getCode() : experior.textarea.value;
+	}
+
+	experior.setCode = function(code) {
+		experior.textarea.disabled ? experior.editor.setCode(code) : experior.textarea.value = code;
+	}
+
+	experior.toggleAutoComplete = function() {
+		experior.editor.autocomplete = (experior.editor.autocomplete) ? false : true;
+	}
+
+
+	experior.toggleReadOnly = function() {
+		experior.textarea.readOnly = (experior.textarea.readOnly) ? false : true;
+		if(experior.style.display != 'none') // prevent exception on FF + iframe with display:none
+			experior.editor.readOnly(experior.textarea.readOnly ? true : false);
+	}
+
+	experior.toggleLineNumbers = function() {
+		var cn = experior.editor.body.className;
+		experior.editor.body.className = (cn==''||cn=='show-line-numbers') ? 'hide-line-numbers' : 'show-line-numbers';
+	}
+
+	experior.toggleEditor = function() {
+		if(experior.textarea.disabled) {
+			experior.textarea.value = getCode();
+			experior.textarea.disabled = false;
+			experior.style.display = 'none';
+			experior.textarea.style.display = 'inline';
+		}
+
+		else {
+			experior.textarea.disabled = true;
+			experior.setCode(experior.textarea.value);
+			experior.editor.syntaxHighlight('init');
+			experior.style.display = 'inline';
+			experior.textarea.style.display = 'none';
+		}
+	}
+
+
+	experior.edit();
+	return experior;
+}
+
+
+
+Experior.run = function() {
+	s = document.getElementsByTagName('script');
+	for(var i=0,n=s.length;i<n;i++) {
+		if(s[i].src.match('experior.js')) {
+			Experior.path = s[i].src.replace('experior.js','');
+		}
+	}
+	
+	var textarea = document.getElementById('experior');
+	var hidden = document.getElementById('hiddenfield'); //gets the hidden field
+	
+			id = textarea.id;
+			eval(id+' = new Experior(textarea, hidden)');
+			textarea.parentNode.insertBefore(eval(id), textarea);	
+}
+
+if(window.attachEvent) {
+	window.attachEvent('onload',Experior.run);
+}
+else window.addEventListener('DOMContentLoaded',Experior.run,false);
+
+
+//Gets the text from the editor to the invisible textarea
+function moveText()
+{
+	hidden.value = experior.getCode();
+}
+
+function saveAndExit()
+{
+	hidden.value = experior.getCode();
+	var form = document.getElementById( "hiddenfieldform" );
+}
+
+function alignClick()
+{	
+	experior.editor.alignStart( experior.getCode() );
+	experior.setOptions();
+	experior.editor.syntaxHighlight('init', hidden.value); //hidden.value henter metodenavn fra hidden field
+}
+
+//Gets the text from the hidden field 
+function moveTextUp()
+{	
+	experior.setCode( hidden.value );
+}
+
+function insertMethod(method)
+{
+	var methods = hidden.value.split("\n");
+	experior.editor.insertMethod(method);
+	experior.contentWindow.focus();
+}
+
+function checkFirstLine()
+{
+	var url = window.location.href;
+	
+	experior.editor.checkFirstLine( url );
+}
